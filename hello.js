7dbf10d09082b2c7194243aa44c45fef9a437ee5
Vedant15-coder <kondvilkarvedant@gmail.com>
2025-06-05 13:04:01 +0530

    learning

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("hello");
+let fill=15;
+fill=18
+fill='this'
+console.log(fill)
+const bal=3.14
+console.log(bal);
+const x=11
+console.log(x)
+if(x%2==0)
+    {
+    console.log("even")
+    }
+ else{
+    console.log("odd")
+ }   
